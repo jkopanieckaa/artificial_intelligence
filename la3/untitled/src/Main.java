fe93c2113bf112485787270d5bf0b14f11e492c4
Judyta Kopaniecka <kj53757>
2024-11-18 17:31:20 +0100

    zadanie 2

--- la3/untitled/src/Main.java
+++ la3/untitled/src/Main.java
@@ -1,15 +1,27 @@
+import sac.State;
+import sac.StateFunction;
+import sac.graph.AStar;
+import sac.graph.GraphSearchAlgorithm;
+import sac.graph.GraphState;
+import sac.graph.GraphStateImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    }
+}
+
+class młynek extends GraphStateImpl{
+    private byte[][] state;
+    int white = 0;
+    int black = 0;
+
+    public mlynek(){
+        state = new byte[3][8];
     }
 }
